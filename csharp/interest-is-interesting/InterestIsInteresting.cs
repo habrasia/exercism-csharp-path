5f7fdadba9a383cd1c735c036b7a951306c36254
Asia <asia_csharpcourse@gmail.com>
2022-04-20 19:17:14 +0200

    InterestIsInteresting

---
+++ csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -0,0 +1,33 @@
+using System;
+
+static class SavingsAccount
+{
+    public static float InterestRate(decimal balance) => balance switch
+    {
+        < 0m => 3.213f,
+        < 1000m => 0.5f,
+        < 5000m => 1.621f,
+        _ => 2.475f
+    };
+
+
+    public static decimal Interest(decimal balance) => decimal.Multiply(balance, Convert.ToDecimal(InterestRate(balance)))/100;
+
+    public static decimal AnnualBalanceUpdate(decimal balance) => balance + Interest(balance);
+
+    public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
+    {
+        int years = 0;
+
+        while (balance < targetBalance)
+        {
+            if (balance < 0)
+            {
+                throw new InvalidOperationException("Negative balance cannot grow interests");
+            }
+            balance = AnnualBalanceUpdate(balance);
+            years++;
+        }
+        return years;
+    }
+}
